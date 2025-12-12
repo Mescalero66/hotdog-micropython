9dd511ac6efb38f894b859f84d269b4ae86a4c57
powertec-jono <jonathon.mclauchlan@power-tec.com.au>
2025-12-12 17:26:21 +1100

    202512121254

--- mmmmmain.py
+++ mmmmmain.py
@@ -13,12 +13,22 @@ SDA_PIN = 6
 SCL_PIN = 7
 ONEWIRE_PIN = 3
 LOAD_RELAY_PIN = 5
-UTC_OFFSET = 10 * 60 * 60
-SLEEP_MINUTES = 0.5
-LATEST_LOG_DATE = None
+### PARAMETERS ###
+UTC_OFFSET = 60 * 60 * 10
+SLEEP_MINUTES = 15
+OUTSIDE_TEMP_LOW = 12
+OUTSIDE_TEMP_HIGH = 15
+INSIDE_TEMP_LOW = 12
+INSIDE_TEMP_HIGH = 20
+### VARIABLES ###
 _current_log_filename = str(None)
+_sleep_duration = 0
+_actual_inside_temp = 0
+_actual_outside_temp = 0
+_actual_outside_humi = 0
+_actual_heater_status = 0
 
-### OBJ SETUP ###
+### OBJECT SETUP ###
 wlan = network.WLAN(network.STA_IF)
 led = Pin(BLUE_LED, Pin.OUT)
 load_relay = Pin(LOAD_RELAY_PIN, Pin.OUT)
@@ -38,6 +48,7 @@ def create_daily_log_file():
     global _current_log_filename
     lt = time.localtime()
     filename = "logs/hotdog_{:04d}{:02d}{:02d}.csv".format(lt[0], lt[1], lt[2])
+    timestamp = "{:04d}{:02d}{:02d}-{:02d}{:02d}{:02d}".format(lt[0], lt[1], lt[2], lt[3], lt[4], lt[5])
     if "logs" not in os.listdir():
         os.mkdir("logs")
     if _current_log_filename != filename:
@@ -47,19 +58,16 @@ def create_daily_log_file():
                 pass
         except OSError:
             with open(_current_log_filename, "a") as text_file:
-                text_file.write("Timestamp,InsideTemp,OutsideTemp,OutsideHumi,HeaterOn,SleepTime,CPUTemp\n")
-    return _current_log_filename
+                text_file.write("Timestamp,InsideTemp,OutsideTemp,OutsideHumi,HeaterOn,SleepDuration,CPUTemp\n")
+    return _current_log_filename, timestamp
 
-def log(logline):
-    lt = time.localtime()
-    filename = create_daily_log_file()
-    timestamp = "{:04d}{:02d}{:02d}-{:02d}{:02d}{:02d}".format(lt[0], lt[1], lt[2], lt[3], lt[4], lt[5])
+def log():
+    filename, timestamp = create_daily_log_file()
+    logline = (f"{_actual_inside_temp:.1f},{_actual_outside_temp:.1f},{_actual_outside_humi:.1f},{_actual_heater_status},{_sleep_duration},{esp32.mcu_temperature():.1f}")
     with open(filename, "a") as text_file:
         text_file.write(f"{timestamp},{(str(logline))} \n")
-    
 
 ### HOUSEKEEPING ###
-print(f"CPU Temp: {esp32.mcu_temperature()}°C")
 # ## WIFI ##
 # wlan.active(False)
 # wlan.active(True)
@@ -77,7 +85,6 @@ print(f"CPU Temp: {esp32.mcu_temperature()}°C")
 #     real_time_clock.datetime(local_time)
 # print(time.localtime())
 
-
 i = 0
 j = 0
 ow_sensor_bus.convert_temp()
@@ -89,14 +96,16 @@ while (i < 10):
             temp_value = ow_sensor_bus.read_temp(UID)
         Temp, Humi = i2c_sensor.get_CHT8305_TEMPERATURE_HUMIDITY()
         print(f"Time: {time.localtime()} - OW: {temp_value:.1f}°C - I2C: {Temp:.1f}°C - Humi: {Humi:.1f}%")
-        log(f"OW: {temp_value:.1f}°C - I2C: {Temp:.1f}°C - Humi: {Humi:.1f}%")
+        log()
         ow_sensor_bus.convert_temp()
         time.sleep(1)
         j += 1
     j = 0
     load_relay.off()
-    log(f"SLEEP [CPU: {esp32.mcu_temperature()}°C]")
+    log()
     # machine.lightsleep(int(SLEEP_MINUTES * 1000 * 60))
-    log(f"WAKE! [CPU: {esp32.mcu_temperature()}°C]")
+    log()
     load_relay.on()
     i += 1
+
+### MAIN LOOP ###
