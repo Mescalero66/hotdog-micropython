ca6db4b0dd1cbbf05b201c608c9e80677115c66d
Mescalero66 <mclauchlanjd@gmail.com>
2025-12-14 09:55:47 +1100

    reduce logging frequency

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ SCL_PIN = 7
 ONEWIRE_PIN = 3
 LOAD_RELAY_PIN = 5
 ### PARAMETERS ###
-UTC_OFFSET = 60 * 60 * 10
+UTC_OFFSET = 60 * 60 * 11   # DST
 MAX_LOG_FILES = 14
 SLEEP_MINUTES = 15
 AVERAGE_READS = 4
@@ -54,12 +54,20 @@ def blink_led():
     time.sleep_ms(100)
     led.on()
 
-def power_spike():
+def blink_led_wifi():
+    for i in range(10):
+        led.off()
+        time.sleep_ms(100)
+        led.on()
+        time.sleep_ms(100)
+        i +=1
+
+def current_spike():
     ble.active(True)
     adv_payload = bytes([0x02, 0x01, 0x06])
     interval_us = 20000
     ble.gap_advertise(interval_us, adv_payload, connectable=False)
-    time.sleep_ms(100)
+    time.sleep_ms(50)
     ble.gap_advertise(0, adv_payload, connectable=False)
     ble.active(False)
 
@@ -109,7 +117,7 @@ def create_daily_log_file():
                 pass
         except OSError:
             with open(_current_log_filename, "a") as text_file:
-                text_file.write("Timestamp,InsideTemp,OutsideTemp,HeaterOn,,,OutsideHumi,SleepDuration,CPUTemp\n")
+                text_file.write("Timestamp,InsideTemp,OutsideTemp,OutsideHumi,HeaterOnOff\n")
     return _current_log_filename, timestamp
 
 def cleanup_old_logs():
@@ -123,11 +131,10 @@ def cleanup_old_logs():
         print(f"Deleted old log: {oldest}")
 
 def log():
-    power_spike()
     filename, timestamp = create_daily_log_file()
     if filename.startswith("hotdog_2000"):
         return
-    logline = (f"{_actual_inside_temp:.1f},{_actual_outside_temp:.1f},{_actual_heater_status},,,{_actual_outside_humi:.1f},{_sleep_duration},{esp32.mcu_temperature():.1f}")
+    logline = (f"{_actual_inside_temp:.1f},{_actual_outside_temp:.1f},{_actual_outside_humi:.1f},{_actual_heater_status}")
     with open(filename, "a") as text_file:
         text_file.write(f"{timestamp},{(str(logline))} \n")
     print(f"{timestamp},{(str(logline))} ")
@@ -163,20 +170,28 @@ def off_loop():
     open_relay()
     outside_ave = []
     inside_ave = []
+    humi_ave = []
     while True:
         ow_sensor_bus.convert_temp()
         time.sleep(1)
-        _actual_outside_temp, _actual_outside_humi = i2c_sensor.get_CHT8305_TEMPERATURE_HUMIDITY()
+        outside_temp, outside_humi = i2c_sensor.get_CHT8305_TEMPERATURE_HUMIDITY()
         for UID in ow_sensor_array:
-            _actual_inside_temp = ow_sensor_bus.read_temp(UID)
-        outside_ave.append(_actual_outside_temp)
-        inside_ave.append(_actual_inside_temp)
-        log()
+            inside_temp = ow_sensor_bus.read_temp(UID)
+        outside_ave.append(outside_temp)
+        inside_ave.append(inside_temp)
+        humi_ave.append(outside_humi)
+        current_spike()
         if (len(outside_ave)) >= AVERAGE_READS or (len(inside_ave)) >= AVERAGE_READS:
             outside_mean = int((sum(outside_ave)) / (len(outside_ave)))
             inside_mean = int((sum(inside_ave)) / (len(inside_ave)))
+            humi_mean = int((sum(humi_ave)) / (len(humi_ave)))
             outside_ave = []
             inside_ave = []
+            humi_ave = []
+            _actual_inside_temp = inside_mean
+            _actual_outside_temp = outside_mean
+            _actual_outside_humi = humi_mean
+            log()
             if (outside_mean <= OUTSIDE_TEMP_LOW) or (inside_mean <= INSIDE_TEMP_LOW):
                 _loop_state = 1
             else:
@@ -192,55 +207,61 @@ def on_loop():
     while True:
         ow_sensor_bus.convert_temp()
         time.sleep(1)
-        _actual_outside_temp, _actual_outside_humi = i2c_sensor.get_CHT8305_TEMPERATURE_HUMIDITY()
+        outside_temp, outside_humi = i2c_sensor.get_CHT8305_TEMPERATURE_HUMIDITY()
         for UID in ow_sensor_array:
-            _actual_inside_temp = ow_sensor_bus.read_temp(UID)
-        outside_ave.append(_actual_outside_temp)
-        inside_ave.append(_actual_inside_temp)
-        log()
+            inside_temp = ow_sensor_bus.read_temp(UID)
+        outside_ave.append(outside_temp)
+        inside_ave.append(inside_temp)
+        humi_ave.append(outside_humi)
+        current_spike()
         if (len(outside_ave)) >= AVERAGE_READS or (len(inside_ave)) >= AVERAGE_READS:
             outside_mean = int((sum(outside_ave)) / (len(outside_ave)))
             inside_mean = int((sum(inside_ave)) / (len(inside_ave)))
+            humi_mean = int((sum(humi_ave)) / (len(humi_ave)))
             outside_ave = []
             inside_ave = []
+            humi_ave = []
+            _actual_inside_temp = inside_mean
+            _actual_outside_temp = outside_mean
+            _actual_outside_humi = humi_mean
+            log()
             if (outside_mean >= OUTSIDE_TEMP_HIGH) or (inside_mean >= INSIDE_TEMP_HIGH):
                 _loop_state = 0
         yield ON_READ_INTERVAL
 
-### HOUSEKEEPING ###
 ## WIFI ##
 wlan.active(False)
 wlan.active(True)
 if not wlan.isconnected():
     wlan.config(txpower=8)
     wlan.PM_NONE
-    # wlan.ifconfig(('192.168.1.19','255.255.255.0','192.168.1.1', '192.168.1.1'))
     wlan.config(reconnects=-1)
     print('connecting to network:', WIFI_SSID)
     wlan.connect(WIFI_SSID, WIFI_PW)
     while not wlan.isconnected():
-        time.sleep(2)
-        print("Status:", wlan.status())
+        blink_led_wifi()
+        print("WLAN Status:", wlan.status())
     print('network config:', wlan.ipconfig('addr4'))
 try:
     print(socket.getaddrinfo("google.com", 80))
     print("DNS OK")
 except Exception as e:
     print("DNS failed:", e)
+    pass
+## CLOCK ##
+ntptime.settime()
+yy, mm, dd, hr, min, sec, wd, yd = time.localtime(time.time() + UTC_OFFSET)
+real_time_clock.datetime((yy, mm, dd, wd, hr, min, sec, 0))
 
-# ## CLOCK ##
-if time.localtime()[0] < 2024:
-    ntptime.settime()
-    local_time = time.localtime(time.time() + UTC_OFFSET)
-    real_time_clock.datetime(local_time)
-
+## HOUSEKEEPING ##
+print(f"Local: {time.localtime()} RTC: {real_time_clock.datetime()}")
 state = off_loop()
 current_loop = 0
+
 ### MAIN LOOP ###
 while True:
     try:
         interval = next(state)
-        # time.sleep(interval)
         for i in range(int(interval)):
             blink_led()
             time.sleep_ms(900)
